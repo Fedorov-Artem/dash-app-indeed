0e781ca5e8431363f221161e0e4c86bb70bcda8e
Artem <artem.v.fedorov@gmail.com>
2024-08-08 20:23:11 +0300

    Added more visualizations. Added filter for dates.

--- dash-app.py
+++ dash-app.py
@@ -6,6 +6,8 @@ import numpy as np
 import dash_bootstrap_components as dbc
 import dash_bootstrap_templates
 from datetime import date
+#from datetime import datetime
+
 
 #import dash_leaflet as dl
 #import dash_leaflet.express as dlx
@@ -14,7 +16,6 @@ import plotly.express as px
 #from plotly import graph_objs as go
 #from plotly.tools import make_subplots
 #from plotly.subplots import make_subplots
-#from datetime import datetime as dt
 
 #dbc_css = "./assets/css/bootstrap.min.css"
 dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
@@ -32,7 +33,9 @@ server = app.server
 
 df = pd.read_csv('to_analysis_indeed.csv')
 df['first_online'] = pd.to_datetime(df['first_online'])
-pd.options.mode.chained_assignment = None
+df['last_online'] = pd.to_datetime(df['last_online'])
+df['day_diff'] = (df['last_online'] - df['first_online']).dt.days
+pd.options.mode.chained_assignment =  None
 
 
 all_types_options = [{"label": "Data Science Jobs", "value": "type_ds"},
@@ -63,7 +66,6 @@ def generate_bar_chart(df_sel):
     df_skill['total'] = df_skill.groupby('skill')['count'].transform('sum')
     df_skill = df_skill.loc[df_skill['total'] > 0]
     df_skill = df_skill.sort_values(['total', 'skill'], ascending=False)[:30]
-    #print(df_skill)
 
     fig = px.bar(df_skill,
                  x='count',
@@ -82,8 +84,8 @@ def generate_bar_chart(df_sel):
 
 def generate_line_chart(df_sel):
     df_sel = df_sel.loc[df_sel['is_unique_text'] > 0]
-    df_sel['week_num'] = df_sel['first_online'].dt.isocalendar().week
     df_dates = df_sel.loc[df_sel['first_online'] > '2024-01-14']
+    df_dates['week_num'] = df_dates['first_online'].dt.strftime('%U').astype(int)
     df_dates = df_dates.groupby('week_num', as_index=False).agg(jobs_count = ("url", "nunique"))
 
     fig = px.line(df_dates,
@@ -164,6 +166,26 @@ def generate_single_bar_degree(df_sel):
     fig.update_layout(margin={"t": 0, "b": 0})
     return fig
 
+def generate_single_bar_recruter(df_sel):
+    df_sel = df_sel.loc[df_sel['is_unique_text'] > 0]
+    df_sel['recr'] = 'From one of Recruter Companies'
+    df_sel.loc[df_sel['is_direct'] == 1, 'recr'] = 'Is Direct'
+    df_sel['x'] = 'Is Direct'
+    fig = px.histogram(df_sel,
+                       y='x',
+                       color='recr',
+                       barnorm='percent',
+                       height=75,
+                       )
+    fig.update_layout(
+        yaxis_title=None,
+        xaxis_title=None,
+        showlegend=False,
+    )
+    fig.update_xaxes(visible=False)
+    fig.update_layout(margin={"t": 0, "b": 0})
+    return fig
+
 def generate_pie_cloud(df_sel):
     df_sel = df_sel.loc[df_sel['is_unique_text'] > 0]
     df_pie = df_sel.groupby('cloud_skills', as_index=False).agg(jobs_count = ("url", "nunique"))
@@ -171,6 +193,18 @@ def generate_pie_cloud(df_sel):
                  values='jobs_count',
                  names='cloud_skills',
                  title='Cloud Skills',
+                 hole=.5
+                 )
+    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    return fig
+
+def generate_pie_viz(df_sel):
+    df_sel = df_sel.loc[df_sel['is_unique_text'] > 0]
+    df_pie = df_sel.groupby('viz_tools', as_index=False).agg(jobs_count = ("url", "nunique"))
+    fig = px.pie(df_pie,
+                 values='jobs_count',
+                 names='viz_tools',
+                 title='Viz Tools',
                  hole=.5
                  )
     fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
@@ -187,13 +221,35 @@ def generate_pie_district(df_sel):
     fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
     return fig
 
+def generate_bar_chart_companies(df_sel):
+    df_sel = df_sel.loc[df_sel['is_unique_text'] > 0]
+    df_sel = df_sel.loc[df_sel['is_direct'] > 0]
+    df_emp = df_sel['company'].value_counts()[:15].reset_index()
+
+    fig = px.bar(df_emp,
+                 x='count',
+                 y='company',
+                 )
+    fig.update_layout(
+        title='Top 15 Employers',
+        yaxis_title=None,
+        xaxis_title=None,
+        showlegend=False
+    )
+    return fig
+
 fig_bar = generate_bar_chart(df)
 fig_line = generate_line_chart(df)
 fig_pie_district = generate_pie_district(df)
 fig_en = generate_single_bar_en(df)
 fig_he = generate_single_bar_he(df)
 fig_edu = generate_single_bar_degree(df)
+fig_recr = generate_single_bar_recruter(df)
 #fig_pie_cloud = generate_pie_cloud(df)
+fig_pie_viz = generate_pie_viz(df)
+fig_bar_companies = generate_bar_chart_companies(df)
+
+
 
 # Layout of Dash App
 app.layout = html.Div(
@@ -238,11 +294,20 @@ app.layout = html.Div(
                                     html.Div([
                                         dcc.DatePickerRange(
                                             id='time-period',
-                                            min_date_allowed=date(2024, 1, 11),
-                                            max_date_allowed=max(df['first_online']),
+                                            min_date_allowed=str(min(df['first_online']).date()),
+                                            max_date_allowed=str(max(df['first_online']).date()),
+                                            start_date=str(min(df['first_online']).date()),
+                                            end_date=str(max(df['first_online']).date()),
                                             #initial_visible_month=date(2017, 8, 5),
-                                            #end_date=date(2017, 8, 25)
+                                            #end_date=date(2017, 8, 25),
+                                            className='dbc'
                                             ),
+                                        dcc.Checklist(
+                                            id="include-older",
+                                            options=['include older'],
+                                            value=['include older'],
+                                            className='dbc'
+                                        ),
                                     ]),
                                 ],
                             ),
@@ -274,12 +339,22 @@ app.layout = html.Div(
                                 dbc.Col([
                                     dbc.Card([
                                         html.P(id='exp-text'),
+                                        html.P(id='av-days-text'),
                                         dcc.Graph(id="single_bar_en", figure=fig_en),
                                         dcc.Graph(id="single_bar_he", figure=fig_he),
-                                        dcc.Graph(id="single_bar_degree", figure=fig_edu)
+                                        dcc.Graph(id="single_bar_degree", figure=fig_edu),
+                                        dcc.Graph(id="single_bar_recr", figure=fig_recr),
                                     ])
                                 ], width=4)
-                            ])
+                            ]),
+                        dbc.Row(
+                            # className="row",
+                            children=[
+                                dbc.Col(html.Div([dcc.Graph(id="bar_companies", figure=fig_bar_companies)]), width=8),
+                                dbc.Col([
+                                    dbc.Col([dcc.Graph(id="pie_viz", figure=fig_pie_viz)]),
+                                ], width=4)
+                            ]),
                     ], width=10
 
                 ),
@@ -300,12 +375,19 @@ app.layout = html.Div(
     Output("single_bar_en", "figure"),
     Output("single_bar_he", "figure"),
     Output("single_bar_degree", "figure"),
+    Output("single_bar_recr", "figure"),
+    Output("bar_companies", "figure"),
+    Output("pie_viz", "figure"),
+    Output("av-days-text", "children"),
     [
         Input("job-type", "value"),
-        Input("all-types", "value")
+        Input("all-types", "value"),
+        Input("time-period", "start_date"),
+        Input("time-period", "end_date"),
+        Input("include-older", "value")
     ],
 )
-def filter_df(job_type, all_types):
+def filter_df(job_type, all_types, start_date, end_date, include_older):
     df_selected = df.copy()
 
     if len(job_type)  > 0:
@@ -315,20 +397,34 @@ def filter_df(job_type, all_types):
         df_selected['total'] = df_selected[all_types].sum(axis=1)
         df_selected = df_selected.loc[df_selected['total'] > 0]
 
+    start_date = date.fromisoformat(start_date)
+    end_date = date.fromisoformat(end_date)
+
+    #print(type(start_date))
+    if len(include_older) > 0:
+        df_selected = df_selected.loc[((df_selected['first_online'].dt.date >= start_date) & (df_selected['first_online'].dt.date <= end_date)) |
+        df_selected['first_online'].isnull()]
+    else:
+        df_selected = df_selected.loc[(df_selected['first_online'].dt.date >= start_date) & (df_selected['first_online'].dt.date <= end_date)]
+
     fig_bar = generate_bar_chart(df_selected)
     fig_line = generate_line_chart(df_selected)
     fig_pie_cloud = generate_pie_district(df_selected)
     fig_en = generate_single_bar_en(df_selected)
     fig_he = generate_single_bar_he(df_selected)
     fig_edu = generate_single_bar_degree(df_selected)
+    fig_recr = generate_single_bar_recruter(df_selected)
+    fig_bar_companies = generate_bar_chart_companies(df_selected)
+    fig_pie_viz = generate_pie_viz(df_selected)
 
     selected_jobs_string = "Vacancies Selected: {:,d}".format(
         len(df_selected) )
 
     exp_text = f"average min experience {df_selected['min_experience'].mean():.2f} years"
+    av_days_text = f"average diff is {df_selected['day_diff'].mean():.2f} days"
 
-    return selected_jobs_string, fig_bar, fig_line, fig_pie_cloud, exp_text, fig_en, fig_he, fig_edu
-
+    return selected_jobs_string, fig_bar, fig_line, fig_pie_cloud, exp_text, fig_en, fig_he, fig_edu, fig_recr,\
+           fig_bar_companies, fig_pie_viz, av_days_text
 
 if __name__ == '__main__':
     app.run_server()
